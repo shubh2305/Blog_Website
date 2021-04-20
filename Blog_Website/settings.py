e95991b6519660ef3a7e2c2c32e54a806e670b30
Shubham Karande <shubhamkarande0501@gmail.com>
2021-04-20 21:47:32 +0530

    7th Commit: Deploying process

--- Blog_Website/settings.py
+++ Blog_Website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'i*3+=9@%zsro15eo67ts5if6t0wvc)0!bw_(fx=am^y25tn91$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
